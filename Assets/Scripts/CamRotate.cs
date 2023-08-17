ee7ecf366ac76e3d50ec4a28146e602339d935c1
dice842 <40335663+dice842@users.noreply.github.com>
2023-08-17 16:20:00 +0900

    feat: 카메라 회전, 플레이어 회전, 플레이어 이동
    카메라, 플레이어 회전 추가
    플레이어 이동 및 점프 추가

---
+++ Assets/Scripts/CamRotate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//마우스 입력에 따라 카메라 회전
+// 속성 : 마우스 입력 X, Y, 속도
+// 1. 마우스 입력을 받는다
+// 2. 마우스 입력에 따라 회전방향 설정
+// 3. 물체를 회전
+public class CamRotate : MonoBehaviour
+{
+    public float speed = 10f;
+    float mx = 0;
+    float my = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. 마우스 입력을 받는다
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        mx += mouseX * speed * Time.deltaTime;
+        my += mouseY * speed * Time.deltaTime;
+
+        my = Mathf.Clamp(my, -90f, 90f);
+
+        Vector3 dir = new Vector3 (-my, mx, 0);
+
+        // 2. 마우스 입력에 따라 회전방향 설정
+        // 3. 물체를 회전
+        transform.eulerAngles = dir;
+
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 키보드 입력에 따른 캐릭터의 이동
+// 속성 : 이동속도
+// 1. 입력을 받는다
+// 2. 이동 방향을 설정
+// 3. 이동 방향으로 이동
+
+// 스페이스를 누르면 수직으로 점프
+
+// 점프 중인지 확인
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10f;
+
+    CharacterController characterController;
+    float gravity = -9.8f;
+    float yVelocity = 0;
+    public float jumpPower = 10;
+    public bool isJumping = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        if (isJumping && characterController.collisionFlags == CollisionFlags.Below)
+        {
+            isJumping = false;
+        }
+        else if (characterController.collisionFlags == CollisionFlags.Below)
+        {
+            yVelocity = 0;
+        }
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            yVelocity = jumpPower;
+            isJumping=true;
+        }
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        dir.y = yVelocity;
+
+        yVelocity = yVelocity + gravity * Time.deltaTime;
+        print(yVelocity);
+        //transform.position += dir * speed * Time.deltaTime;
+
+        characterController.Move(dir * speed * Time.deltaTime);
+    }
+}
